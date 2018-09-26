3a955b9a6d888b2b3bfaa39d0d5a48edf7c259a5
Fernando Garcia <fernando.garcia@etermax.com>
2018-09-26 16:46:13 -0300

    Winning and Losing conditions: FeedBack cuando moris, creo imagen de fin

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 
-public string EndText = "Ganaste";
+    public string EndText = "Ganaste";
+    public Image EndScreen;    
     
     public void MakeDead()
     {
         EndText = "YouDied";
         EndGame();
+        EndScreen.color = Color.white;  // con esto me habilita la imagen que deshabilite con los alpha
+//        Instantiate(EndScreen, transform.position, Quaternion.identity);
         Destroy(gameObject);
     }
 
