eade58df23143ed66415d26029e4b55834acf126
Fernando Garcia <fernando.garcia@etermax.com>
2018-09-26 11:59:05 -0300

    Partículas como feedback

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -2,10 +2,12 @@
 
 public class CoinController : MonoBehaviour
 {
+    public GameObject reward;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
+            Instantiate(reward, transform.position, Quaternion.identity); //lo instancio en mi position sin rotation(quaternion)
             other.gameObject.GetComponent<PlayerInventory>().AddCoins();
 
             //Destroy(gameObject); // me destruye solo la moneda, pero me deja el cartel de su padre (coinBase)
